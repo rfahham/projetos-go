790e1b2497d80f46815dda92abdbcc8f9a68d275
rfahham <rfahham@hotmail.com>
2024-05-15 20:42:20 -0300

    tipos de dados

---
+++ src/tipos/tipos.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Printf("Type: %T - Value: %v\n", true, true)
+	fmt.Printf("Type: %T - Value: %v\n", "fahham", "fahham")
+	fmt.Printf("Type: %T - Value: %v\n", 1, 1)
+	fmt.Printf("Type: %T - Value: %v\n", "1", "1")
+	fmt.Printf("Type: %T - Value: %v\n", 1.317, 1.317)
+	
+}
